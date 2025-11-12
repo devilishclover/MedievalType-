bee9e5285683c0c19b77f3803ab3e0dd9f392dfe
James Richardson <devilishclover@gmail.com>
2025-11-11 22:41:21 -0700

    autoplay doesnt work lol oh well

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import guyImage from './assets/guy.png'
 function App() {
   return (
     <div className="main">
-      <audio loop autoPlay muted>
-      <source src={medievalMusic} type="audio/mpeg" />
-    </audio>
       <Typingtest />
       <div className='keyboard-container'>
         <img src={guyImage} alt="guy" />
         <Keyboard />
         <p className="note">Note: If a letter appears not to be able to be typed, it is due to the font making certain characters difficult to understand. You'd better study up on your medieval fonts peasant!</p>
+        <audio controls loop>
+      <source src={medievalMusic} type="audio/mpeg" />
+    </audio>
       </div>
       
     </div>
